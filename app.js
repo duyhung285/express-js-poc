e766c7d25d6e0e4b93ccd8d5b8e4500d7cf3150d
Trinh Hung <trinh.hung@imhelo.com>
2019-01-16 10:45:00 +0700

    Update

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
- let multer  = require('multer');
- let express = require('express');
- let app     = express();
+let multer = require('multer');
+let express = require('express');
+let app = express();
 
- var storage = multer.diskStorage({
-     destination: (req, file, cb) => {
-         cb(null, '/uploaded')
-     },
-     filename: (req, file, cb) => {
-         cb(null, file.fieldname + '-' + Date.now())
-     }
- });
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '/uploaded')
+    },
+    filename: (req, file, cb) => {
+        cb(null, "File " + '-' + Date.now())
+    }
+});
 
- var upload2 = multer({storage: storage});
+var upload2 = multer({storage: storage});
 
- let upload  = multer({ storage: multer.memoryStorage() });
+let upload = multer({storage: multer.memoryStorage()});
 
- app.post('/api/upload', upload.single('sample'), (req, res) => {
-   if (req.file) {
-       console.log('Uploading file...');
-       var filename = req.file.filename;
-       console.log('get file ' + filename);
-       res.send({ result: 'success'});
+app.post('/api/upload', upload.single('sample'), (req, res) => {
+    if (req.file) {
+        console.log('Uploading file...');
+        var filename = req.file.originalname;
+        console.log('get file ' + filename);
+        res.send({result: 'success'});
     } else {
         console.log('No File Uploaded');
-        res.send({ result: 'fail'});
+        res.send({result: 'fail'});
     }
- });
+});
 
- app.post('/api/upload2', upload2.single('sample'), (req, res) => {
-     if (req.file) {
-         console.log('Uploading file...');
-         var filename = req.file.filename;
-         console.log('get file ' + filename);
-         res.send({ result: 'success'});
-     } else {
-         console.log('No File Uploaded');
-         res.send({ result: 'fail'});
-     }
- });
+app.post('/api/upload2', upload2.single('sample'), (req, res) => {
+    if (req.file) {
+        console.log('Uploading file...');
+        var filename = req.file.filename;
+        console.log('get file ' + filename);
+        res.send({result: 'success'});
+    } else {
+        console.log('No File Uploaded');
+        res.send({result: 'fail'});
+    }
+});
 
- app.post('/array', upload.array('sample'), (req, res) => {
-   console.log(req.body)
-   console.log(req.files);
-   res.send();
- });
+app.post('/array', upload.array('sample'), (req, res) => {
+    console.log(req.body)
+    console.log(req.files);
+    res.send();
+});
 
- app.get('/', (req, res) => {
-     res.send("Server is running at 2019 port");
- });
+app.get('/', (req, res) => {
+    res.send("Server is running at 2019 port");
+});
 
- app.listen(2019);
+app.listen(2019);
